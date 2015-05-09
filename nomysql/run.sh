913b511f2caab614445af46c2623ab26f97f8165
Steve Nolen <technolengy@gmail.com>
2015-05-08 18:14:38 -0700

    add container for no-mysql build. add container for all-in-one build

---
+++ nomysql/run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+# with no mysql installed, assume these have been set properly.
+# DB_HOST=${DB_HOST:-localhost}
+# DB_PORT=${DB_PORT:-3306}
+# DB_NAME=${DB_NAME:-ohmage}
+# DB_USER=${DB_USER:-ohmage}
+# DB_PASS=${DB_PASS:-ohmage}
+FQDN=${FQDN:-$HOSTNAME}
+ 
+# update ohmage conf to point to correct db
+sed -i "s/^db.jdbcurl.*/db.jdbcurl=jdbc:mysql:\/\/$DB_HOST:$DB_PORT\/$DB_NAME?characterEncoding=utf8/" /etc/ohmage.conf
+sed -i "s/^db.username.*/db.username=$DB_USER/" /etc/ohmage.conf
+sed -i "s/^db.password.*/db.password=$DB_PASS/" /etc/ohmage.conf
+ 
+# update flyway conf
+# note that the placeholders wont be updated at each boot.
+echo "flyway.url=jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME
+flyway.user=$DB_USER
+flyway.password=$DB_PASS
+flyway.placeholders.fqdn=$FQDN
+flyway.placeholders.base_dir=/var/lib/ohmage" > /var/lib/ohmage/flyway/conf/flyway.conf
+
+# if [ $DB_HOST == 'localhost' ]; then
+#   service mysql start
+#   mysql -uroot mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME; grant all on $DB_NAME.* to \"$DB_USER\"@\"$DB_HOST\" IDENTIFIED BY \"$DB_PASS\"; FLUSH PRIVILEGES;"
+# fi
+ 
+# execute migrations
+/var/lib/ohmage/flyway/flyway migrate
+ 
+# start tomcat in foreground
+/usr/local/tomcat/bin/catalina.sh run
