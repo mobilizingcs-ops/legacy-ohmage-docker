c76518d6671fe31a7bba63320ecfa688d379860c
Steve Nolen <technolengy@gmail.com>
2015-05-11 14:48:12 -0700

    mostly refactor to two images

--- platform/run.sh
+++ platform/run.sh
@@ -2,9 +2,16 @@
 
 set -e
 
-# set environment variables
-DB_HOST=${DB_HOST:-localhost}
-DB_PORT=${DB_PORT:-3306}
+# if a container is linked as 'mysql', this should exist
+if [ -n "$MYSQL_ENV_MYSQL_ROOT_PASSWORD" ]; then
+  DB_HOST=$MYSQL_PORT_3306_TCP_ADDR
+  DB_PORT=$MYSQL_PORT_3306_TCP_PORT
+else
+  #assume no linked container. 
+  DB_HOST=localhost
+  DB_PORT=3306
+fi
+# use passed env variable or defaults
 DB_NAME=${DB_NAME:-ohmage}
 DB_USER=${DB_USER:-ohmage}
 DB_PASS=${DB_PASS:-ohmage}
@@ -23,15 +30,19 @@ flyway.password=$DB_PASS
 flyway.placeholders.fqdn=$FQDN
 flyway.placeholders.base_dir=/var/lib/ohmage" > /var/lib/ohmage/flyway/conf/flyway.conf
 
-if [ $DB_HOST == 'localhost' ]; then
+# create database stuffz. different depending on linked mysql container.
+if [ -n "$MYSQL_ENV_MYSQL_ROOT_PASSWORD" ]; then
+  mysql -uroot -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h$DB_HOST -P$DB_PORT mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME; grant all on $DB_NAME.* to \"$DB_USER\"@\"%\" IDENTIFIED BY \"$DB_PASS\"; FLUSH PRIVILEGES;"
+else
   service mysql start
   mysql -uroot mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME; grant all on $DB_NAME.* to \"$DB_USER\"@\"$DB_HOST\" IDENTIFIED BY \"$DB_PASS\"; FLUSH PRIVILEGES;"
+
 fi
 
 # start nginx
 chmod -R 0744 /var/www/webapps/survey
 service nginx start
- 
+
 # execute migrations
 /var/lib/ohmage/flyway/flyway migrate
  
